1f1e6d61998822adda3372e4cfe9c43dbee20c8e
James Walker <walkah@walkah.net>
2013-10-25 17:52:05 -0400

    build-essential doesn't work on Mavericks, and I'm not even using it

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,11 @@ maintainer_email 'walkah@walkah.net'
 license          'Apache 2.0'
 description      'Installs/Configures my workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.8.1'
+version          '0.8.2'
 
 supports "mac_os_x"
 
 depends "apt",             "~> 2.0.0"
-depends "build-essential", "~> 1.4.2"
 depends "homebrew"         
 depends "homesick",        "~> 0.4.0"
 depends "mac_os_x",        "~> 1.4.2"
