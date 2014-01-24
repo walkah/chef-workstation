25617df2eb5c70d9a7cb5ae7405ce3e3054405f9
James Walker <walkah@walkah.net>
2014-01-23 20:42:41 -0500

    git-up has rvm/bundler issues - switching to python port

--- attributes/ruby.rb
+++ attributes/ruby.rb
@@ -32,7 +32,6 @@ node.default['rvm']['user_installs'] = [
     'global_gems'     => [
       { "name" => "bundler"},
       { "name" => "chef"},
-      { "name" => "git-up" },
       { "name" => "homesick" },
       { "name" => "lunchy" },
       { "name" => "tmuxinator" }
