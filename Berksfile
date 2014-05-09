fdbf80ad72c1800ffe7ad172c157b2cf9b71c0b9
James Walker <walkah@walkah.net>
2014-05-09 12:05:26 -0400

    looks like brew cask made it in (finally)

--- Berksfile
+++ Berksfile
@@ -2,5 +2,4 @@ source "http://api.berkshelf.com"
 
 metadata
 
-cookbook 'homebrew', :git => 'https://github.com/walkah/chef-homebrew.git', :branch => 'master'
 cookbook 'rvm', :git => 'https://github.com/fnichol/chef-rvm.git', :branch => 'master'
--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email 'walkah@walkah.net'
 license          'Apache 2.0'
 description      'Installs/Configures my workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.9.13'
+version          '0.9.14'
 
 supports 'mac_os_x'
 
 depends 'apt',             '~> 2.0.0'
-depends 'homebrew'
+depends 'homebrew',        '~> 1.6.4'
 depends 'homesick',        '~> 0.4.0'
 depends 'mac_os_x',        '~> 1.4.2'
 depends 'nvm',             '~> 0.1.0'
