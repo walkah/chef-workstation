4cdb405ae1dc32f90960d634ae363056e79b4424
James Walker <walkah@walkah.net>
2014-09-24 16:37:56 -0400

    version bumpin'

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,11 @@ maintainer_email 'walkah@walkah.net'
 license 'Apache 2.0'
 description 'Installs/Configures my workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.9.18'
+version '0.9.19'
 
 supports 'mac_os_x'
 
-depends 'apt', '~> 2.4.0'
-depends 'homebrew', '~> 1.8.0'
+depends 'homebrew', '~> 1.9.0'
 depends 'homesick', '~> 0.4.0'
 depends 'mac_os_x', '~> 1.4.6'
 depends 'nvm', '~> 0.1.0'
