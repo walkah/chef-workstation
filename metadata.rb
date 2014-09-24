7b81d9a93b4e6a50c42706978e95195c101559f3
James Walker <walkah@walkah.net>
2014-09-24 17:00:58 -0400

    shh. foodcritic

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,9 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.9.19'
 
 supports 'mac_os_x'
+supports 'ubuntu'
 
+depends 'apt'
 depends 'homebrew', '~> 1.9.0'
 depends 'homesick', '~> 0.4.0'
 depends 'mac_os_x', '~> 1.4.6'
