77c79844110d5834f47b349f1e6bed477cfd251a
James Walker <walkah@walkah.net>
2015-01-13 11:37:44 -0500

    Updates!

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'walkah@walkah.net'
 license 'Apache 2.0'
 description 'Installs/Configures my workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.10.6'
+version '0.10.7'
 
 supports 'mac_os_x'
 supports 'ubuntu'
 
 depends 'apt'
-depends 'homebrew', '~> 1.9.2'
+depends 'homebrew', '~> 1.11.0'
 depends 'homesick', '~> 0.4.0'
 depends 'mac_os_x', '~> 1.4.6'
 depends 'nvm', '~> 0.1.0'
