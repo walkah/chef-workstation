f1f0da9e0a75cb34c56ee1652e6932b3f5dd649c
James Walker <walkah@walkah.net>
2016-04-05 23:19:09 -0400

    add source / issues urls to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,8 @@
 name 'workstation'
 maintainer 'James Walker'
 maintainer_email 'walkah@walkah.net'
+source_url 'https://github.com/walkah/chef-workstation'
+issues_url 'https://github.com/walkah/chef-workstation/issues'
 license 'Apache 2.0'
 description 'Installs/Configures my workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
