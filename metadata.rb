1c48a30b7e2c5e2dc75b4220fb4e7d790045c487
James Walker <walkah@walkah.net>
2016-06-10 15:04:53 -0400

    version bumps

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url 'https://github.com/walkah/chef-workstation/issues'
 license 'Apache 2.0'
 description 'Installs/Configures my workstation'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.13.0'
+version '0.13.1'
 
 supports 'mac_os_x'
 supports 'ubuntu'
